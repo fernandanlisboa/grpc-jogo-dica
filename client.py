18bf8b75db89c7715021369edfa8136700d1ed2a
Fernanda Lisboa <50326541+fernandanlisboa@users.noreply.github.com>
2022-04-02 01:19:38 -0300

    adicionando palpites e criterio de fim de jogo e dinamica do fornecimento e impressão das dicas

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 import threading
 from urllib import response
+import time
 import grpc
 import dica_pb2_grpc
 import dica_pb2
+
+empty = dica_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
 
 
 class Client:
@@ -12,16 +15,65 @@ class Client:
         channel = grpc.insecure_channel('localhost:50051')
         self.stub = dica_pb2_grpc.DicaServiceStub(channel)
         self.__entrar_jogo()
-        threading.Thread(target=self.__escutar_jogo).start()
+        # threading.Thread(target=self.__escutar_jogo).start()
         # threading.Thread(target=self.__l)
+        self.__escutar_jogo()
+        time.sleep(5)
+        self.__escutar_rodada()
 
     def __escutar_jogo(self):
+        palavra = ''
         print('Esperando Jogadores')
-        response = self.stub.PartidaStream(dica_pb2.MensagemVazia())
+        response = self.stub.PartidaStream(empty)
+        print(f'response:{response}')
         if(response.nome == self.nome):
-            palavra = str(input('É a sua vez de escolher a palavra'))
-            self.stub.EscolherPalavra(dica_pb2.Palavra(palavra=palavra))
+            palavra = str(
+                input('É a sua vez de escolher a palavra!\nPalavra: '))
+            response_palavra = self.stub.EscolherPalavra(
+                dica_pb2.Palavra(palavra=palavra))
+            print(
+                f'A palavra que você escolheu foi: {response_palavra.palavra}')
 
+    # tá dando problema quando entra nessa função, mesmo se comentar o RodadaStream
+    def __escutar_rodada(self):
+        # inicia = False
+        # while inicia == False:
+        response = self.stub.RodadaStream(empty)
+        print(f'{response.nome} quem dará a dica!')
+        # inicia = response.inicia
+
+        # if inicia == True:
+
+        response_dica = ''
+        response_palpite = ''
+        response_enviar_dica = ''
+        if response.nome == self.nome:
+            response_palavra = self.stub.VerPalavra(empty)
+            print(f"A palavra é: {response_palavra.palavra}")
+            dica = input('Forneça a dica: ')
+            response_enviar_dica = self.stub.DarDica(dica_pb2.Dica(dica=dica))
+        else:
+            # TODO colocar um try except no loop enquanto a dica não é fornecida
+            print(f'{response.nome} está digitando a dica!')
+        # TODO um loop para esperar nos outros clientes enquanto quem dá a dica n fornece
+
+        response_dica = self.stub.VerDica(empty)
+        print(f"{response.nome} deu a dica: {response_dica.dica}")
+
+        if response_enviar_dica.nome == self.nome:
+            palpite = input(f'{self.nome}, digite seu palpite: ')
+            self.stub.DarPalpite(
+                dica_pb2.DarPalpite(palpite=palpite))
+        else:
+            print(f'{response_enviar_dica.nome} está digitando o palpite!')
+
+        response_palpite = self.stub.VerPalpite(empty)
+        print(f'O palpite enviado foi {response_palpite.palpite}')
+        if response_palpite.acertou == True:
+            # TODO uma função no server que retorna os nomes de todos os jogadores para poder mostrar a dupla que ganhou!
+            print(f'A palavra é: {response_palpite.palpite}!')
+            print((f'Parabéns! {response_enviar_dica.nome} ganhou o jogo!!!'))
+            # response = self.stub.VerPalpite(dica_pb2.VerPalpite(empty))
 
     def __entrar_jogo(self):
         response = self.stub.CriarJogador(
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from queue import Empty
 import grpc
 import time
 from concurrent import futures
@@ -7,52 +8,113 @@ import dica_pb2
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+
 class DicaServer(dica_pb2_grpc.DicaServiceServicer):
 
-    def __init__(self) :
+    def __init__(self):
         self.palavra = ''
         self.numJogadores = 4
         self.jogadores = []
         self.dicas = []
+        self.palpites = []
         self.vez = 0
+        self.fim = False
 
     def PartidaStream(self, request, context):
         print('Esperando Jogadores')
-        while len(self.jogadores) < 2:
+        while len(self.jogadores) < 4:
             espera = True
-            #fica aqui paradinho
+            # fica aqui paradinho
 
         print('calma')
-        print('Dupla 1: ' + self.jogadores[0] + ' e ' + self.jogadores[1])
-        #print('Dupla 2: ' + self.jogadores[1] + ' e ' + self.jogadores[3])
+        # msg = ''
+        # for k in range(self.numJogadores//2):
+        #     # msg += f''
+        #     # if k % 2 == 0:
+        #     msg = f'Dupla {k +1}: {self.jogadores[k]} e {self.jogadores[k+2]}'
+        # else:
+        #     print(msg)
+        #     msg = ''
+        print('Dupla 1: ' + self.jogadores[0] + ' e ' + self.jogadores[2])
+        print('Dupla 2: ' + self.jogadores[1] + ' e ' + self.jogadores[3])
 
-        #trocar por sorteio
+        # TODO trocar por sorteio
         primeiro_jogador = 0
+        self.vez = primeiro_jogador
+        print('response: ', dica_pb2.NomeJogadorResp(
+            nome=self.jogadores[primeiro_jogador], recebida=True))
+
         return dica_pb2.NomeJogadorResp(nome=self.jogadores[primeiro_jogador], recebida=True)
 
+    def RodadaStream(self, request, context):
+        comeca = False
+        # espera por 10 segundos -> tempo que o jogador leva para digitar
+        # time.sleep(10)
+        while self.palavra == '':
+            # espera enquanto a palavra não é escolhida
+            # time.sleep(1)
+            print('Esperando...')
+            espera = True
+        # comeca = True
+        print(f'A rodada vai começar! {self.palavra}')
+        return dica_pb2.NomeJogadorResp(nome=self.jogadores[self.vez], recebida=True)
+
+        # return dica_pb2.RodadaResposta(inicia=comeca)
+
+    #     # while self.fim != True:
 
     def CriarJogador(self, request, context):
         self.jogadores.append(request.nome)
         return dica_pb2.NomeJogadorResp(nome=request.nome, recebida=True)
 
     def EscolherPalavra(self, request, context):
+        self.vez += 1
         self.palavra = request.palavra
-        return dica_pb2.PalavraResp(nome=self.palavra, recebida=request.recebida)
+
+        return dica_pb2.PalavraResp(palavra=self.palavra, recebida=True)
 
     def VerPalavra(self, request, context):
-        return dica_pb2.PalavraResp(nome=self.palavra, recebida=request.recebida)
-    
-    def Palpitar(self, request, context):
+        # print('vendo palavra')
+        return dica_pb2.PalavraResp(palavra=self.palavra, recebida=True)
+
+    def DarDica(self, request, context):
+        #TODO verificações de palavras repetidas
+        dica = request.dica
+        self.dicas.append(dica)
+
+        indice = self.vez+2
+
+        return dica_pb2.NomeJogadorResp(nome=self.jogadores[indice], recebida=True)
+
+    def VerDica(self, request, context):
+        #TODO verificação se a lista foi atualizada: out of range, caso seja a mesma palavra da rodada anterior
+        
+        print(self.vez)
+        print(f'Dica: {self.dicas[-1]}')
+        return dica_pb2.Dica(dica=self.dicas[-1])
+
+    def DarPalpite(self, request, context):
+        #TODO verificações de palavras repetidas
         '''
         recebe o palpite enviado pelo jogador e retorna se é igual ao da palavra escolhida
         request: requisição do servidor
         '''
-        palpite = request.palpite
+        self.palpites.append(request.palpite)
+
+        if self.palavra == request.palpite:
+            self.fim = True
+        else:
+            if self.vez == 1:
+                self.vez = 0
+            else:
+                self.vez = 1
+
         
-        if self.palavra == palpite:
-            return True
-        
-        return False
+
+    def VerPalpite(self, request, context):
+        '''
+        '''
+        return dica_pb2.PalpiteResposta(palpite=self.palpites[-1], acertou=self.fim, recebeu=True)
 
 
 def serve():
